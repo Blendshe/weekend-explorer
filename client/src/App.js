df846a2aff4fc45d643742ffd5f3a0772759d9b5
JaneG <getwize@googlemail.com>
2023-05-16 20:20:55 +0100

    made changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [outdoorswim, setOutdoorSwim] = useState([]);
+
+  async function getOutdoorSwimData() {
+    const API = "http://localhost:8080/outdoorswim";
+    const res = await axios.get(API);
+    setOutdoorSwim(res.data);
+  }
+
   return (
     <div className="App">
       <h1> Weekend Project </h1>
+      <button onClick={getOutdoorSwimData}>Get information</button>
+      {outdoorswim.map(function (member, index) {
+        return (
+          <p key={index}>
+            {member.where} is where you'll find the swim spot {member.name}
+          </p>
+        );
+      })}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors"); // middleware - think of it as the bridge between the client and server
+const cors = require("cors"); // middleware
 require("dotenv").config();
 const app = express();
 app.use(cors());
@@ -10,14 +10,15 @@ const data = require("./data.json");
 
 app.get("/", function (request, response) {
   response.json(
-    "You can use the /family endpoint to retrieve data about my family."
+    "You can use the /outdoor-swim endpoint to retrieve data about locations."
   );
 });
 
-app.get("/family", function (request, response) {
+app.get("/outdoor-swim", function (request, response) {
   response.json(data);
 });
 
 app.listen(PORT, function () {
   console.log("Server is listening on PORT " + PORT);
+
 });
